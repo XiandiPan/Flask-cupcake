a89a70db6841115f6df87735f0c19cfb0c98f1e8
Xiandi Pan <cindypan1807@gmail.com>
2023-05-01 15:11:50 -0400

    part one

--- models.py
+++ models.py
@@ -1,1 +1,50 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+IMG_DEFAULT = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True,
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False,
+        unique=True,
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False,
+        unique=False,
+    )
+
+    image_url = db.Column(
+        db.String(500), #ask 
+        nullable=False,
+        unique=False,
+        default=IMG_DEFAULT
+    )
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
